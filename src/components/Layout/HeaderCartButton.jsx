f24134de78aa524e4dee8c148e6077514c3b4198
harshal <harshalc562@gmail.com>
2023-05-22 11:30:31 +0530

    add effects when new items is added

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,25 +1,34 @@
-import React, {useContext, useState, useEffect} from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 import cartContext from '../../store/cart-context'
 
 const HeaderCartButton = (props) => {
-    const[btnIsHighlighted, setBtnIsHighlighted] = useState(false )
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
     const cartCtx = useContext(cartContext)
     console.log(cartCtx);
-    const numberOfCartContext = cartCtx.items.reduce((curNumber, item) => {
+
+    const { items } = cartCtx;
+
+    const numberOfCartContext = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0)
 
-    const  { items } = cartCtx;
     const btnClasses = `${classes.button}  ${btnIsHighlighted ? classes.bump : ''}`
 
     useEffect(() => {
-        if(cartCtx.items.length === 0){
+        if (items.length === 0) {
             return;
         }
         setBtnIsHighlighted(true)
-    }, [ items ])
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false)
+        }, 300)
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items])
 
     return (
         <button className={btnClasses} onClick={props.onClick}>
